dcbd79cab29936be6dc45a0244644fbf1c66beb1
Jani Jegoroff <jani.jegoroff@gmail.com>
2015-01-16 11:59:37 +0000

    Unit tests for ListView

--- spec/spec_list_view.rb
+++ spec/spec_list_view.rb
@@ -11,6 +11,26 @@ class SpecListView < Minitest::Spec
   end
 
   describe 'ListView' do
-    # Nothing here yet
+    describe 'ListView.count' do
+      it 'should call Calabash query method with correct parameters' do
+        $stub_query_response = [1]
+        ListView.count.must_equal(1)
+        $uiquery.must_equal('ListView')
+        $args.first.must_equal(:count)
+
+        $stub_query_response = [3]
+        ListView.count.must_equal(3)
+        $uiquery.must_equal('ListView')
+        $args.first.must_equal(:count)
+      end
+    end
+
+    describe 'ListView.scroll_to_index' do
+      it 'should call Calabash query method with correct parameters' do
+        ListView.scroll_to_index(10)
+        $uiquery.must_equal('ListView')
+        $args.first.must_equal({:smoothScrollToPosition=>10})
+      end
+    end
   end
 end
