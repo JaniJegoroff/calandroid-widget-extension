d22d788787441ba9933a9f60a1846102eb0dbadf
Jani Jegoroff <jani.jegoroff@gmail.com>
2015-01-16 11:59:04 +0000

    scroll_to_index method for ListView

--- lib/calandroid-widget-extension/list_view.rb
+++ lib/calandroid-widget-extension/list_view.rb
@@ -9,5 +9,10 @@ class ListView < Base
       q = parse_query(aIdOrIndex)
       Calabash::Android::Operations.query(q, :count).first
     end
+
+    def scroll_to_index(aIndex)
+      q = parse_query(nil)
+      Calabash::Android::Operations.query(q, smoothScrollToPosition: aIndex)
+    end
   end
 end
